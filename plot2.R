1cadc6cc368dcccc9cf73bc71c0c876f7874a24b
gkum <ghanshyamkumawat@gmail.com>
2015-10-11 14:00:04 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -20,5 +20,5 @@ plot(data_need$Global_active_power~data_need$Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
 
 #save file
-dev.copy(png, file="Plot2.png", height=480, width=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()

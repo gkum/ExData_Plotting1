7d08e02607fd3a48da6dc26d8c8e9c617107b105
gkum <ghanshyamkumawat@gmail.com>
2015-10-11 13:59:47 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -14,5 +14,5 @@ data_need <- data_epc[(data_epc$Date =="2007-2-1")|(data_epc$Date =="2007-2-2"),
 hist(data_need$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
 #save to file
-dev.copy(png, file="Plot1.png", height=480, width=480)
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
